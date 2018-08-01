a0634ee12f8db8bbe4c4edb4302d0c0d14029899
Kapil Gupta <er.kapilgupta1990@gmail.com>
2018-08-01 20:19:37 +0530

    Self join

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -46,3 +46,38 @@ delete from student group by name,salary having count(*)>1;
   -- Note below query will not work on postgres , else it is perfectly working fine on all other database.
   delete from student stu1 left join (select min(id) as id from student  group by name,salary having count(*)>1)abc  on stu1.id=abc.id
   and abc.id is not null;
+  
+  
+  
+ =============Self Join====================== 
+  
+  Self Join:
+Problem1 : Print which employee belongs to which manager?
+Problem2 : Print the Manager who is having more then 2 reportee?
+Problem 3: Print employee hierarchy tree?
+
+create table employee(
+empId integer,
+empName varchar(20),
+managerId integer  
+);
+insert into employee values(1,'kapil',2);
+insert into employee values(2,'mahesh',1);
+insert into employee values(3,'ramesh',2);
+insert into employee values(4,'suresh',3);
+insert into employee values(5,'suresh',2);
+
+
+Solution 1:
+select emp.empName as Employee ,man.empName as Manager
+from employee emp join employee man
+on emp.managerId=man.empId
+order by emp.empId
+
+
+Solution 2:
+select man.empName as Manager
+from employee emp join employee man
+on emp.managerId=man.empId
+group by emp.managerId
+having (count(emp.managerId)>2)
